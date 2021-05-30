838abbeb89315350a48b6278396ba90efc10936e
annopackage <anno.reasearch.cv@gmail.com>
2021-05-30 21:09:09 +0800

    check comments

--- scripts/make_dataset_local.sh
+++
@@ -1,4 +0,0 @@
-# coco
-# python tools/make_datasets_local.py --dataset coco --root_dir data --data_dir coco/val2017
-# mpii
-python tools/make_datasets_local.py --dataset mpii --root_dir data --data_dir mpii/images
